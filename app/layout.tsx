a478e72ffed672339e6be63efc30a72caa31d79d
jantay04 <bzantaev@gmail.com>
2024-04-09 22:39:22 +0200

    The beginning of the architecture change in FSD

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ReactNode } from "react"
 
-import { Toaster } from "@/components/ui/toaster";
-import "@stream-io/video-react-sdk/dist/css/styles.css";
-import "react-datepicker/dist/react-datepicker.css";
-import "./globals.css";
+import { Toaster } from "@/components/ui/toaster"
+import "@stream-io/video-react-sdk/dist/css/styles.css"
+import "react-datepicker/dist/react-datepicker.css"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "tudom",
+  title: "YOOM",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
